b251f2a54cd6c64c8935433e4520c18ae241aa66
Tomasz Kowalczyk <tomek@kownet.info>
2017-05-12 10:34:45 +0200

    run manually or with DI

--- FacadeNotifier/FacadeNotifier.App/Program.cs
+++ FacadeNotifier/FacadeNotifier.App/Program.cs
@@ -2,10 +2,12 @@
 {
     using Autofac;
     using Core.Channels;
+    using Core.Clients;
     using DI;
     using Main;
     using NLog;
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     class Program
@@ -14,25 +16,41 @@
 
         static void Main(string[] args)
         {
-            var container = ContainerConfig.Configure();
-
-            using (var scope = container.BeginLifetimeScope())
+            #region Run manually
+            var channels = new List<IChannel>()
             {
-                var app = scope.Resolve<IApplication>();
+                new HipChatChannel(new HipChatClient(
+                    new Uri("https://api.hipchat.com/v2/"),
+                    roomToken: "",
+                    messageToken: "")),
+                new SlackChannel(new SlackClient(
+                    new Uri("")))
+            };
 
-                var channelTypes = scope.GetImplementingTypes<IChannel>();
+            new Application().Run(channels);
+            #endregion
 
-                var channels = channelTypes.Select(t => scope.Resolve(t) as IChannel);
+            #region Run with DI (Autofac)
+            //var container = ContainerConfig.Configure();
 
-                try
-                {
-                    app.Run(channels);
-                }
-                catch (Exception ex)
-                {
-                    _logger.Error(ex.Message);
-                }
-            }
+            //using (var scope = container.BeginLifetimeScope())
+            //{
+            //    var app = scope.Resolve<IApplication>();
+
+            //    var channelTypes = scope.GetImplementingTypes<IChannel>();
+
+            //    var channels = channelTypes.Select(t => scope.Resolve(t) as IChannel);
+
+            //    try
+            //    {
+            //        app.Run(channels);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        _logger.Error(ex.Message);
+            //    }
+            //}
+            #endregion
 
             Console.ReadLine();
         }
